22aa8ad671a453b6e227b93c44ffa3fafcfd2163
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-19 21:08:27 -0400

    bug fixed
    the through statement of the productTag is the middleman that both has tag_id and product_id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,16 +11,13 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Product,
-        //Helped needed here
-        //through: ProductTag,
-        attributes: ["id", "product_name", "price", "stock", "category_id"],
-        include: 
-          [{
-            model: ProductTag,
-            attributes: ["id", "product_id", "tag_id"]
-          }]
-      }
-    ]
+        //It's doing two JOIN statements here by joining
+        //1. Tag with ProductTag referenced to tag_id
+        //2. ProductTag with Product referenced to product.id
+        //Through productTag because it is the middleman that both has tag_id and product_id.
+        through: ProductTag,
+      },
+    ],
   })
     .then((tags) => res.json(tags))
     .catch((err) => res.status(500).json(err));
@@ -30,74 +27,74 @@ router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   Tag.findOne({
-    where: {id: req.params.id},
+    where: { id: req.params.id },
     attributes: ["id", "tag_name"],
     include: [
       {
         model: Product,
         //Helped needed here
-        //through: ProductTag,
-        attributes: ["id", "product_name", "price", "stock", "category_id"],
-        include: 
-          [{
-            model: ProductTag,
-            attributes: ["id", "product_id", "tag_id"]
-          }]
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: "No tag found with this id" });
       }
-    ]
-  })
-  .then((tag) => {
-    if(!tag){
-      res.status(404).json({ message: "No tag found with this id"});
-    }
-    res.json(tag);
-  })
-  .catch((err) => {
-    res.status(500).json(err);
-  });
+      res.json(tag);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
   Tag.create({
-    tag_name: req.body.tag_name
+    tag_name: req.body.tag_name,
   })
-  .then((tag) => {res.json(tag)})
-  .catch((err) => {res.status(500).json(err)})
+    .then((tag) => {
+      res.json(tag);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update({
-    tag_name: req.body.tag_name
-  },
-  {
-    where: {id: req.params.id}
-  })
-  .then((tag)=>{
-    if(!tag){
-      res.status(404).json({message:'No tag found with this id'});
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: { id: req.params.id },
     }
-    res.json(tag);
-  })
-  .catch((err)=>{
-    console.log(err);
-    res.status(500).json(err);
-  })
+  )
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: "No tag found with this id" });
+      }
+      res.json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
   Tag.destroy({
-    where: {id: req.params.id}
+    where: { id: req.params.id },
   })
-  .then((tag)=>{
-    if(!tag){
-      res.status(404).json({message:'No tag found with this id'});
-    }
-    res.json(tag);
-  })
-  .catch((err)=> res.status(500).json(err));
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: "No tag found with this id" });
+      }
+      res.json(tag);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
